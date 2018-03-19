752b588794271d3299956c56bdf3c30e087f681b
lenziten <lenziten@hotmail.com>
2018-03-19 23:33:33 +0800

    增加计价功能

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,10 +5,10 @@ const app = getApp()
 Page({
   data: {
     text:"首页信息展示",
-    menus: ['menu1', 'menu2', 'menu3', 'menu4', 'menu5', 'menu6', 'menu7', 'menu8', 'menu9'],
+    menus: [{ name: 'menu1', price: 1.10, count: 0 }, { name: 'menu2', price: 2.30, count: 0 }, { name: 'menu3', price: 3.00, count: 0 }, { name: 'menu4', price: 4.40, count: 0 }, { name: 'menu5', price: 5.00, count: 0 }, { name: 'menu6', price: 6.66, count: 0 }, { name: 'menu7', price: 7.00, count: 0 }, { name: 'menu8', price: 8.56, count: 0 }, { name: 'menu9', price: 9.00, count: 0 }],
     toView: 'red',
     scrollTop: 1,
-    count: [],
+    totalPrice: 0
   },
   editData:function(data_){
     this.setData({ menus: data_});
@@ -45,7 +45,28 @@ Page({
   onUnload:function(){//页面关闭
 
   },
-  clkMinute:function(options){
-
+  clkMinute: function (event) {
+    var index = event.currentTarget.dataset.index;
+    var menus_ = this.data.menus;
+    if (null == menus_[index].count || menus_[index].count == 0) {
+      return;
+    }
+    menus_[index].count--;
+    var totalPrice_ = this.data.totalPrice;
+    totalPrice_ = totalPrice_ - menus_[index].price;
+    this.setData({ menus: menus_, totalPrice: totalPrice_ });
+  },
+  clkPlus: function (event) {
+    var index = event.currentTarget.dataset.index;
+    var menus_ = this.data.menus;
+    if (null == menus_[index].count) {
+      return;
+    }
+    menus_[index].count++;
+    var totalPrice_ = this.data.totalPrice;
+    console.log(totalPrice_ + " + " + menus_[index].price);
+    totalPrice_ = totalPrice_ + menus_[index].price;
+    console.log("= " + totalPrice_);
+    this.setData({ menus: menus_, totalPrice: totalPrice_ });
   }
 })
